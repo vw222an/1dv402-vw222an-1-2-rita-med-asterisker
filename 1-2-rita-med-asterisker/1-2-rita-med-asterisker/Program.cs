bed33c54555613195be0596d0c643ee2a546c2b8
vw222an <vw222an@student.lnu.se>
2014-09-19 16:54:42 +0200

    Got the colors to work but the spacing is still...
    Got the colors to work but the spacing is still wrong.

--- 1-2-rita-med-asterisker/1-2-rita-med-asterisker/Program.cs
+++ 1-2-rita-med-asterisker/1-2-rita-med-asterisker/Program.cs
@@ -10,20 +10,36 @@ namespace _1_2_rita_med_asterisker
     {
         static void Main(string[] args)
         {
+            int colorSwitch = 0;
+
+
             for (int col = 0; col < 39; col++)
             {
                 Console.WriteLine();
 
                 for (int row = 0; row < 25; row++)
                 {
-                    Console.Write("* ");
-                    if(row == 0)
+                    switch (colorSwitch = col % 3)
                     {
-                    Console.Write(" ");
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+
+                    }
+
+                    Console.Write("* ");
+                    if (row == 0)
+                    {
+                        Console.Write(" ");
                     }
                 }
-           }
-            Console.WriteLine();
+            }
         }
     }
 }
