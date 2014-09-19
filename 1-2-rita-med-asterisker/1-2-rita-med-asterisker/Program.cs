6d56ad802bfe29fed4642870f88fa705bf7122e8
vw222an <vw222an@student.lnu.se>
2014-09-19 14:23:48 +0200

    Got the stars out but still need to fix...
    Got the stars out but still need to fix the spacing and the colors.

---
+++ 1-2-rita-med-asterisker/1-2-rita-med-asterisker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_rita_med_asterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int col = 0; col < 39; col++)
+            {
+                Console.WriteLine();
+
+                for (int row = 0; row < 25; row++)
+                {
+                    Console.Write("* ");
+                    if(row == 0)
+                    {
+                    Console.Write(" ");
+                    }
+                }
+           }
+            Console.WriteLine();
+        }
+    }
+}
